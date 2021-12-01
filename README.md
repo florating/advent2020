d2d0bb546655ed76370faec5e7e1eed9992aea31
Flora Ting <ting.flora@gmail.com>
2021-11-30 22:30:58 -0800

    Complete day1 of 2021; add doctests; refactor

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# advent2020
+# advent of code
+https://adventofcode.com/
+
+## advent2020
+https://adventofcode.com/2020
+
+I previously completed days 1-8 of Advent of Code 2020 back in December 2020... when I didn't yet know much Python.
+
+As of November 2021, I have just completed the full-time SWE course at Hackbright Academy. It's interesting to see how my coding approach has changed!
+
+
+## advent2021
+https://adventofcode.com/2021
+
+Let's see if I can do this alongside the 100 Days of Code challenge!
---
+++ advent2021/day01.py
@@ -0,0 +1,49 @@
+"""Day 1: submarines, measurements, and sliding windows"""
+
+
+def read_input(file, convert_to_int=False):
+    text = []
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            text.append(int(line)) if convert_to_int else text.append(line)
+    return text
+
+
+def count_increases(data):
+    """Count the # of times there is an increase when comparing consecutive measurement data.
+    >>> test = 'test01.txt'
+    >>> test_nums = read_input(test, convert_to_int=True)
+    >>> count_increases(test_nums)
+    7
+    >>> test_sums = sum_measures(test_nums)
+    >>> count_increases(test_sums)
+    5
+    """
+    count = 0
+    prev = data[0]
+    for num in data[1:]:
+        if num > prev:
+            count += 1
+        prev = num
+    return count
+
+
+def sum_measures(nums):
+    """Calculate 3-measurement sliding windows using nums (list).
+    EG: i = 0, 4, 8, ... all mark the index position for the start of the 3-measure window
+    """
+    sums = []
+    i, j, k = 0, 1, 2
+    while k < len(nums):
+        sums.append(nums[i] + nums[j] + nums[k])
+        i, j, k = j, k, k + 1
+    return sums
+
+
+if __name__ == '__main__':
+    file = 'input01.txt'
+    lst = read_input(file, convert_to_int=True)
+    print(f'The answer to part 1 is: {count_increases(lst)}')
+
+    sums = sum_measures(lst)
+    print(f'The answer to part 2 is: {count_increases(sums)}')
