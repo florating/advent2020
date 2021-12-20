c1588fe9b3d82addb5e6240eb4d897f60c64037f
Flora Ting <ting.flora@gmail.com>
2021-12-19 23:38:36 -0800

    Refactor

--- advent2021/day07.py
+++ advent2021/day07.py
@@ -10,13 +10,12 @@ def find_meetup(nums):
     modes = statistics.multimode(nums)
     sd = math.ceil(statistics.pstdev(nums))
     print(f'modes = {modes}')
+    print(f'sd = {sd}')
 
     values = set()
     for mode in modes:
         for i in range(sd):
             values.update({abs(mode + i), abs(mode - i)})
-
-    print(values)
 
     for value in values:
         total_fuel = 0
@@ -32,10 +31,6 @@ def calculate_fuel(origin, target):
     """Calculate arithmetic sum:
     sum(a + kd) = n/2(2a + (n - 1)d)
     """
-    # fuel = 0
-    # for i in range(abs(target - origin)):
-    #     fuel += i + 1
-    # return fuel
     n = abs(target - origin)
     a = d = 1
     total = (n / 2) * (2 * a + (n - 1) * d)
@@ -45,7 +40,9 @@ def calculate_fuel(origin, target):
 if __name__ == '__main__':
     file = 'inputs/input07.txt'
     # file = 'test_inputs/test07.txt'
+
     data = open(file, 'r').readline().strip().split(',')
     positions = list(map(int, data))
-    print(positions)
-    print(find_meetup(positions))
+
+    fuel, meetup_spot = find_meetup(positions)
+    print(f'Use {fuel} fuel to get to meetup_spot = {meetup_spot}')
