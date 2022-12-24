b4d89b617ee73c116df5ad85ad0383032175c51d
Flora Ting <ting.flora@gmail.com>
2022-12-24 03:44:18 -0800

    Complete day 10 (part 2) - will refactor The final drawing is incorrect for each row at position 39.

--- advent2022/day10.py
+++ advent2022/day10.py
@@ -14,39 +14,34 @@ class CPU:
 
     def parse(self, code_line):
         """Only increase cycle count for 'noop'."""
-        self.cycles += 1
-        self.draw()
+        self.tick()
 
         if code_line[:4] == 'addx':
-            self.record_signal()
-
-            self.cycles += 1
-            self.draw()
+            self.tick()
             self.x += int(code_line.strip().split()[-1])  # v
 
+    def tick(self):
+        self.cycles += 1
+        self.draw()
         self.record_signal()
 
     def record_signal(self):
         self.signals[self.cycles] = self.x
 
-        # only for testing with 10_small.txt
-        # self.print_signal(self.cycles)
-
     def get_signal(self, cycle_num):
         return self.signals[cycle_num]
 
-    def calculate_signal_strength(self, cycle_num, is_during=True):
-        if is_during:
-            return cycle_num * self.get_signal(cycle_num - 1)
+    def calculate_signal_strength(self, cycle_num):
         return cycle_num * self.get_signal(cycle_num)
 
     def print_signal(self, cycle_num):
         print(f'cycle = {cycle_num}, X = {self.get_signal(cycle_num)}')
 
     def draw(self):
-        if self.cycles % 40 == 1:
+        position = self.cycles % 40 - 1
+        if position == 0:
             self.drawing += '\n'
-        if abs(self.cycles - self.x) <= 1:
+        if abs(position - self.x) <= 1:
             self.drawing += '#'
         else:
             self.drawing += '.'
@@ -66,19 +61,28 @@ def main(text):
     ans = 0
     for val in CYCLES:
         curr = cpu.calculate_signal_strength(val)
-        print(f'cycle = {val}, signal strength = {curr}')
+        # print(f'cycle = {val}, signal strength = {curr}')
         ans += curr
 
-    # for i in range(216, 240):
-    #     cpu.print_signal(i)
-
-    cpu.print_signal(10)
     print(f'final cycle = {cpu.cycles}, final X = {cpu.x}')
     print(cpu.drawing)
     return ans
 
 
 if __name__ == '__main__':
-    # program = read_input('inputs/10.txt')
-    program = read_input('test_inputs/10.txt')
+    program = read_input('inputs/10.txt')
+    # program = read_input('test_inputs/10.txt')
     print(main(program))
+
+"""
+###..#....####.####.####.#.....##..#####
+#..#.#....#.......#.#....#....#..#.#...#
+#..#.#....###....#..###..#....#....###..
+###..#....#.....#...#....#....#.##.#...#
+#.#..#....#....#....#....#....#..#.#...#
+#..#.####.####.####.#....####..###.####.
+
+It's messing up the final position of each row somehow... at position 39
+
+The answer is not 'RLEZFLGB'. The solution was 'RLEZFLGE'.
+"""
